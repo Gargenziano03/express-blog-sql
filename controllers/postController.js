0403e29852ce2e4eaf7f128f257fa9abf73220d1
Gargenziano03 <gargenziano03@gmail.com>
2024-12-06 15:29:33 +0100

    add fix

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,11 +1,12 @@
-const posts = require('../db/posts.js')
+//const posts = require('../db/posts.js')
 const fs = require('fs')
-const connection = require('../db/db.js')
+const connection = require('../db/db')
 
 const index = (req, res) => {
-    const sql = 'SELECT * FROM blog'
+    const sql = 'SELECT * FROM posts'
     connection.query(sql, (err, results) => {
-        if (err) return res.status(500).json({ error: ee });
+        if (err) return res.status(500).json({ error: 'Database query failed' });
+
         const responseData = {
             data: results,
             counter: results.length
