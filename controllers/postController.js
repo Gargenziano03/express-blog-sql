6b585ecdee97abeaad7223c9d0ace9118f2a672a
Gargenziano03 <gargenziano03@gmail.com>
2024-12-06 17:11:06 +0100

    add .env

--- controllers/postController.js
+++ controllers/postController.js
@@ -18,11 +18,7 @@ const index = (req, res) => {
 };
 
 const show = (req, res) => {
-    const post = posts.find((post) => post.title.toLowerCase() === req.params.title)
-    if (!post) {
-        return res.status(404).json({ error: 'no post found with tha title' })
-    }
-    return res.status(200).json({ data: post })
+
 };
 
 
--- db/db.js
+++ db/db.js
@@ -1,10 +1,11 @@
 const mysql = require('mysql2');
-
 const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'Bellapocho.03',
-    database: 'blog_db'
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    port: process.env.DB_PORT
+
 });
 connection.connect((err) => {
     if (err) throw err;
